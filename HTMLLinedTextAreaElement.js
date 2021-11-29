3bdae7b24627623f623fe5722feee33e0b81112d
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-11-29 23:14:29 +0900

    Add files via upload

--- HTMLLinedTextAreaElement.js
+++ HTMLLinedTextAreaElement.js
@@ -10,34 +10,36 @@ customElements.define('lined-textarea', class extends HTMLElement {
     const root = this.attachShadow({mode: 'open'});
 
     const link = document.createElement('link');
+    const linenumbers = document.createElement('div');
+    const editor = document.createElement('textarea');
+    const slot = document.createElement('slot');
+
+    this.#linenumbers = linenumbers;
+    this.#editor = editor;
+
+    root.appendChild(link);
+    root.appendChild(linenumbers);
+    root.appendChild(editor);
+    root.appendChild(slot);
+
     link.href = 'HTMLLinedTextAreaElement.css';
     link.rel = 'stylesheet';
-    root.appendChild(link);
 
-    const linenumbers = document.createElement('div');
     linenumbers.className = 'linenumbers';
     linenumbers.innerHTML = this.#updateLineNumbers();
-    root.appendChild(linenumbers);
 
-    const editor = document.createElement('textarea');
     editor.className = 'editor';
     const onchange = event => this.#updateLineNumbers();
     editor.oninput = onchange;
     editor.onpaste = onchange;
     editor.wrap = 'off';
-    root.appendChild(editor);
 
-    const slot = document.createElement('slot');
     slot.addEventListener('slotchange', event => {
       const [node] = slot.assignedNodes();
       editor.value = node.nodeValue;
       node.nodeValue = '';
       editor.oninput();
     });
-    root.appendChild(slot);
-
-    this.#linenumbers = linenumbers;
-    this.#editor = editor;
   }
 
   #updateLineNumbers () {
